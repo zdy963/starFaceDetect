fce34f8e301697890b02b465470ac7a5bfc8f3dd
DONGYU <zhangdongyu963@outlook.com>
2019-03-07 04:03:54 +0800

    Add a little bit font-end

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ import tensorflow as tf
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 modelpath = 'fullset.model'
-
+labelpath = 'fullset.lb'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'JPG', 'PNG'])
 
 with open('config.json') as config:
     app.secret_key = json.load(config)['app_secret']
@@ -24,7 +25,6 @@ def loadModel():
     tf.reset_default_graph()
 
     global lb
-    labelpath = 'transfull.lb'
     lb = pickle.loads(open(labelpath, "rb").read())
 
     global graph
@@ -55,6 +55,10 @@ def extractFaceCor2(image):
     return cor
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -69,58 +73,63 @@ def upload():
     """
     # Get image
     print('[INFO] Receiving image from front-end')
-    img = request.files.get('image').read()
+    img = request.files.get('image')
+    if img and allowed_file(img.filename):
+        img = request.files.get('image').read()
 
-    # Change it into opencv form
-    image = cv2.imdecode(np.fromstring(img, np.uint8), 1)
-    image = cv2.pyrDown(image)
+        # Change it into opencv form
+        image = cv2.imdecode(np.fromstring(img, np.uint8), 1)
+        image = cv2.pyrDown(image)
 
-    # Copy an output
-    output = image.copy()
+        # Copy an output
+        output = image.copy()
 
-    # Extract face from image
-    cor = extractFaceCor2(image)
-    face = image[cor[1]:cor[1] + cor[3], cor[0]:cor[0] + cor[2]]
-    cv2.imwrite('face.jpg', face)
-    print('[INFO] Face extracted success!', cor)
+        # Extract face from image
+        cor = extractFaceCor2(image)
+        face = image[cor[1]:cor[1] + cor[3], cor[0]:cor[0] + cor[2]]
+        cv2.imwrite('face.jpg', face)
+        print('[INFO] Face extracted success!', cor)
 
-    # Change the attrs for image to fit the model
-    print('[INFO] Resizing face image')
-    # face = cv2.resize(face, (224, 224))
-    face = cv2.resize(face, (128, 128))
-    face = face.astype("float") / 255.0
-    face = img_to_array(face)
-    face = np.expand_dims(face, axis=0)
+        # Change the attrs for image to fit the model
+        print('[INFO] Resizing face image')
+        # face = cv2.resize(face, (224, 224))
+        face = cv2.resize(face, (128, 128))
+        face = face.astype("float") / 255.0
+        face = img_to_array(face)
+        face = np.expand_dims(face, axis=0)
 
-    # Predict similar stars
-    print('[INFO] Predicting similar stars')
-    model = load_model(modelpath)
-    # with graph.as_default():
-    proba = model.predict(face, verbose=1)[0]
-    print(proba)
+        # Predict similar stars
+        print('[INFO] Predicting similar stars')
+        model = load_model(modelpath)
+        # with graph.as_default():
+        proba = model.predict(face, verbose=1)[0]
+        print(proba)
 
-    # Get similar stars name and percentage
-    labels = []
-    for i in range(3):
-        # print(i)
-        idx = np.argmax(proba)
-        print(idx)
-        curr_prob = proba[idx]
-        proba[idx] = 0
-        label = lb.classes_[idx]
+        # Get similar stars name and percentage
+        labels = []
+        for i in range(3):
+            # print(i)
+            idx = np.argmax(proba)
+            print(idx)
+            curr_prob = proba[idx]
+            proba[idx] = 0
+            label = lb.classes_[idx]
 
-        # build the label and draw the label on the image
-        result = {'label': label, 'prob': '%.2f' % (curr_prob*100) + '%'}
-        labels.append(result)
+            # build the label and draw the label on the image
+            result = {'label': label, 'prob': '%.2f' % (curr_prob*100) + '%'}
+            labels.append(result)
 
-    # Decode image
-    curr_time = str(time.time())
-    randomNum = random.randint(0, 100)  # 生成的随机整数n，其中0<=n<=100
-    uniqueName = 'img/' + curr_time + str(randomNum) + '.jpg'
-    cv2.imwrite('static/'+uniqueName, output)
+        # Decode image
+        curr_time = str(time.time())
+        randomNum = random.randint(0, 100)  # 生成的随机整数n，其中0<=n<=100
+        uniqueName = 'img/' + curr_time + str(randomNum) + '.jpg'
+        cv2.imwrite('static/'+uniqueName, output)
 
-    # Return data to front-end
-    return render_template('show.html', img_path=uniqueName, pred=labels)
+        # Return data to front-end
+        return render_template('show.html', img_path=uniqueName, pred=labels)
+
+    else:
+        return render_template('alertInfo.html', text='Please submit image', redirectTo='/')
 
 
 if __name__ == '__main__':
